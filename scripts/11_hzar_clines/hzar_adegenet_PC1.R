8439b2f76f28b5e35f67911f00cf516047e09341
slager <dave.slager@gmail.com>
2019-09-06 13:20:37 -0700

    change "pca1" to "strQ" in script and variables

--- scripts/11_hzar_clines/hzar_adegenet_PC1.R
+++ scripts/11_hzar_clines/hzar_adegenet_PC1.R
@@ -1,6 +1,6 @@
 ## RUN THE TWO SCRIPTS IN THIS ORDER TO MAKE mtDNA nuDNA comparison GRAPH:
 # 1) hzar_mtDNA.R
-# 2) hzar_adegenet_PC1.R
+# 2) hzar_strQ.R
 
 library(adegenet)
 library(hzar)
@@ -61,7 +61,7 @@ hzar.meta.fix(clineModel)$muL <- TRUE
 hzar.meta.fix(clineModel)$muR <- TRUE
 
 fitRequest <- hzar.first.fitRequest.gC(gModel=clineModel,obsData=mydata,verbose=F)
-myfitlist_pca1 <- hzar.chain.doSeq(hzar.request=fitRequest,count=3,collapse=F)
+myfitlist_strQ <- hzar.chain.doSeq(hzar.request=fitRequest,count=3,collapse=F)
 
 
 '#CC3311' -> red
@@ -71,19 +71,19 @@ rbPal <- colorRampPalette(c(blue,red))
 
 #This adds a column of color values
 # based on the y values
-pca1_colors <- rbPal(100)[as.numeric(cut(means,breaks = 100))]
+strQ_colors <- rbPal(100)[as.numeric(cut(means,breaks = 100))]
 mtDNA_colors <- rbPal(100)[as.numeric(cut(pops$mtDNA_A,breaks = 100))]
 
 
 
 pdf("cline_both2.pdf",5.5,5.5,useDingbats=F)
 par(mar=c(4.1,4.1,4.1,2.6))
-hzar.plot.cline(myfitlist_pca1[[3]],xlab="Pacific coastline (km)",ylab="Population value",xlim=c(0,7000),pch=NA,add=F)
+hzar.plot.cline(myfitlist_strQ[[3]],xlab="Pacific coastline (km)",ylab="Population value",xlim=c(0,7000),pch=NA,add=F)
 hzar.plot.cline(myfitlist_mtDNA[[3]],add=T,lty=2,pch=NA)
 # Get rid of line past points
 rect( max(dists), .95, 7100, 1.05, col='white', lty=0)
 rect( min(dists), -.05, -100, .05, col='white', lty=0)
-points(dists,means,pch=3,col=pca1_colors) #plot pca1 data
+points(dists,means,pch=3,col=strQ_colors) #plot strQ data
 points(pops$dist,pops$mtDNA_A,pch=1,col=mtDNA_colors) #plot mtDNA data
 abline(v=c(3542,4864,5185,5656),lty=3,col='darkgray')
 mtext(at=c(1671,4203,5024,5420,6428),text=c('AK','BC','A','OR','CA'),side=1,line=-1,cex=0.8)
@@ -92,7 +92,7 @@ mtext(at=c(5024),text=c('W'),side=1,line=-1.7,cex=0.8)
 legend(x=0,y=1.1,c("mtDNA fr(American)","Genomic ancestry"),lty=c(2,1),pch=c(1,3),bg="white",cex=.9,bty="n")
 dev.off()
 
-#myfitlist_pca1[[3]]$mcmcRaw %>% tail
+#myfitlist_strQ[[3]]$mcmcRaw %>% tail
 
 # AK-BC  3542
 # BC-WA  4864
@@ -107,17 +107,17 @@ dev.off()
 # =
 
 #Fit to data group to enable next steps
-hzar.fit2DataGroup(myfitlist_pca1[[3]]) -> fit3_pca1
+hzar.fit2DataGroup(myfitlist_strQ[[3]]) -> fit3_strQ
 
 #PDF graph version
-#pdf("pca1_cline_cred.pdf",6,6)
-#hzar.plot.fzCline(fit3_pca1,xlab="Smoothed coastline distance in km (AK to CA)",ylab="adegenet PC1",main="hzar.plot.cline()")
-#hzar.plot.cline(myfitlist_pca1[[3]],xlab="Smoothed coastline distance in km (AK to CA)",ylab="adegenet PC1",main="hzar.plot.cline()")
+#pdf("strQ_cline_cred.pdf",6,6)
+#hzar.plot.fzCline(fit3_strQ,xlab="Smoothed coastline distance in km (AK to CA)",ylab="adegenet PC1",main="hzar.plot.cline()")
+#hzar.plot.cline(myfitlist_strQ[[3]],xlab="Smoothed coastline distance in km (AK to CA)",ylab="adegenet PC1",main="hzar.plot.cline()")
 #dev.off()
 
-hzar.get.ML.cline(myfitlist_pca1[[3]])$param.all$width
-hzar.get.ML.cline(myfitlist_pca1[[3]])$param.all$center
+hzar.get.ML.cline(myfitlist_strQ[[3]])$param.all$width
+hzar.get.ML.cline(myfitlist_strQ[[3]])$param.all$center
 
 #Extract center & +/- 2 LL range
-hzar.getLLCutParam(fit3_pca1,"width",2)
-hzar.getLLCutParam(fit3_pca1,"center",2)
+hzar.getLLCutParam(fit3_strQ,"width",2)
+hzar.getLLCutParam(fit3_strQ,"center",2)
